77ecf83f8cd17d52a3fc38217fe32e14bfd84446
dnovotny <dnovotny@fb.com>
2022-09-06 04:36:20 -0700

    fixed show_co3d_dataset.py example

--- examples/show_co3d_dataset.py
+++ examples/show_co3d_dataset.py
@@ -10,22 +10,14 @@ from tqdm import tqdm
 from omegaconf import DictConfig
 from typing import Tuple
 
-import pytorch3d
 from co3d.utils import dbir_utils 
 from pytorch3d.renderer.cameras import CamerasBase
-from pytorch3d.implicitron.dataset.dataset_base import FrameData
-from pytorch3d.implicitron.dataset.dataset_map_provider import DatasetMap
 from pytorch3d.implicitron.dataset.json_index_dataset import JsonIndexDataset
 from pytorch3d.implicitron.dataset.json_index_dataset_map_provider_v2 import (
     JsonIndexDatasetMapProviderV2
 )
 from pytorch3d.implicitron.tools.config import expand_args_fields
-
-# implicitron trainer is not a part of pytorch3d so we include it here
-_pytorch3d_root = os.path.dirname(os.path.dirname(pytorch3d.__file__))
-implicitron_trainer_dir = os.path.join(_pytorch3d_root, "projects", "implicitron_trainer")
-sys.path.insert(0, implicitron_trainer_dir)
-from visualize_reconstruction import render_sequence
+from pytorch3d.implicitron.models.visualization.render_flyaround import render_flyaround
 
 
 DATASET_ROOT = os.getenv("CO3DV2_DATASET_ROOT")
@@ -38,6 +30,17 @@ def main(
     output_dir: str = os.path.join(os.path.dirname(__file__), "show_co3d_dataset_files"),
     n_show_sequences_per_category: int = 2,
 ):
+    """
+    Visualizes object point clouds from the CO3D dataset.
+
+    Note that the code iterates over all CO3D categories and (by default) exports
+    2 videos per a category subset. Hence, the whole loop will run for
+    a long time (3-4 hours).
+    """
+
+    # log info messages
+    logging.basicConfig(level=logging.INFO)
+
     # make the output dir
     os.makedirs(output_dir, exist_ok=True)
 
@@ -45,15 +48,13 @@ def main(
     with open(os.path.join(DATASET_ROOT, "category_to_subset_name_list.json"), "r") as f:
         category_to_subset_name_list = json.load(f)
     
+    # iterate over the co3d categories
     categories = sorted(list(category_to_subset_name_list.keys()))
-
     for category in tqdm(categories):
 
         subset_name_list = category_to_subset_name_list[category]
 
         for subset_name in [
-            "fewview_test",
-            "manyview_test",
             "fewview_dev",
             "manview_dev_0",
         ]:
@@ -61,10 +62,11 @@ def main(
             if subset_name not in subset_name_list:
                 continue
 
+            # obtain the dataset
+            expand_args_fields(JsonIndexDatasetMapProviderV2)
             dataset_map = JsonIndexDatasetMapProviderV2(
                 category=category,
                 subset_name=subset_name,
-                dataset_root=DATASET_ROOT,
                 test_on_train=False,
                 only_test_set=False,
                 load_eval_batches=True,
@@ -104,29 +106,28 @@ def main(
                     os.makedirs(os.path.dirname(video_path), exist_ok=True)
 
                     logger.info(f"Rendering rotating video {video_path}")
-
-                    render_sequence(
+                    
+                    render_flyaround(
                         train_dataset,
                         sequence_name,
                         model,
                         video_path,
-                        n_eval_cameras=40,
+                        n_flyaround_poses=40,
                         fps=20,
-                        max_angle=2 * math.pi,
                         trajectory_type="circular_lsq_fit",
-                        trajectory_scale=1.1,
+                        max_angle=2 * math.pi,
+                        trajectory_scale=1.5,
                         scene_center=(0.0, 0.0, 0.0),
                         up=(0.0, -1.0, 0.0),
                         traj_offset=1.0,
-                        n_source_views=9,
-                        viz_env="show_co3d_dataset",
-                        visdom_show_preds=False,
-                        visdom_server="http://127.0.0.1",
-                        visdom_port=8097,
-                        num_workers=10,
-                        seed=None,
-                        video_resize=None,
-                        visualize_preds_keys=("images_render",),
+                        n_source_views=1,
+                        visdom_show_preds=True,
+                        visdom_environment="show_co3d_dataset",
+                        visualize_preds_keys=(
+                            "images_render",
+                            "masks_render",
+                            "depths_render",
+                        ),
                     )
 
 
@@ -156,6 +157,7 @@ class PointcloudRenderingModel(torch.nn.Module):
             camera[[0]],
             self._render_size,
             self._pointcloud,
+            point_radius=0.01,
         )
         return {
             "images_render": render.image_render,
